ebdc1750f24683769c28a17e6a08a169ad6277bf
zzzang12 <wkddnjs3268@ajou.ac.kr>
2020-09-11 01:04:19 +0900

    clone

--- cloned/static/predict.js
+++ cloned/static/predict.js
@@ -19,7 +19,7 @@ $( document ).ready(async function () {
 	modelLoaded = false;
 	$('.progress-bar').show();
     console.log( "Loading model..." );
-    model = await tf.loadGraphModel('model/model.json');
+    model = await tf.loadLayersModel('model/model.json');
     console.log( "Model loaded." );
 	$('.progress-bar').hide();
 	modelLoaded = true;
